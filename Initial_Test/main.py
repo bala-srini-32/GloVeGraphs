fd8b4d99ed919b63656839fb07b4fec79a99cb52
sudhre23 <sus046@eng.ucsd.edu>
2017-06-10 02:19:56 -0700

    Merge:

--- Initial_Test/main.py
+++ Initial_Test/main.py
@@ -76,7 +76,7 @@ def read_graph():
 		if args.weighted:
 			G = nx.read_edgelist(args.input, nodetype=int, data=(('weight',float),), create_using=nx.DiGraph())
 		else:
-			G = nx.read_edgelist(args.input, nodetype=int, create_using=nx.DiGraph())
+			G = nx.read_edgelist(args.input, nodetype = int, create_using=nx.DiGraph())
 			for edge in G.edges():
 				G[edge[0]][edge[1]]['weight'] = 1
 
